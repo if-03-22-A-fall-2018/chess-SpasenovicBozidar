bd32a93de9ba92e62482a00d86a28f47a1dadc62
Bozidar Spasenovic <spasenovic2002@gmail.com>
2018-11-12 13:32:01 +0100

    Not compiling

--- assignment/chess.h
+++ assignment/chess.h
@@ -10,3 +10,54 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+ enum PieceType {
+   Pawn, Rook, Knight, Bishop,
+   Queen, King, NoPiece
+ };
+ enum PieceColor { White, Black };
+ enum MoveType { NormalMove, CaptureMove };
+ struct  	ChessPiece{
+   enum PieceColor color;
+   enum PieceType type;
+ };
+ struct	ChessSquare{
+   bool is_occupied;
+   struct ChessPiece type;
+ };
+
+
+ typedef struct ChessSquare 	ChessBoard [8][8];
+
+
+typedef char 	File;
+typedef int 	Rank;
+
+
+
+
+struct ChessSquare* get_square (ChessBoard chess_board, File file, Rank rank);
+struct ChessPiece	get_piece (ChessBoard chess_board, File file, Rank rank);
+
+
+
+
+
+
+
+
+
+bool 	is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type);
+void 	init_chess_board (ChessBoard chess_board);
+
+bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank);
+bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece);
+
+void 	setup_chess_board (ChessBoard chess_board);
+bool 	remove_piece (ChessBoard chess_board, File file, Rank rank);
+bool 	squares_share_file (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+bool 	squares_share_rank (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+bool 	squares_share_diagonal (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+bool 	squares_share_knights_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+bool 	squares_share_pawns_move (enum PieceColor color, enum MoveType move, File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
